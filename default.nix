580730c9b8f3b6a102c2ae8e5f4b12ec8147dfe0
Eduard Burtescu <edy.burt@gmail.com>
2015-09-22 22:30:35 +0300

    Reduce default.nix to bare mininum.

--- default.nix
+++ default.nix
@@ -1,23 +1,7 @@
 let
    pkgs = import <nixpkgs> {};
- in rec {
-   r3Env = pkgs.stdenv.mkDerivation rec {
-     name = "r3-env";
-     buildInputs = with pkgs; with pkgs.xlibs; [
-        stdenv gcc5
-        pkgconfig
-
-        # glfw deps
-        cmake mesa libXrandr libXi libXxf86vm libXfixes x11
-
-        mesa_noglu
-
-        libXcursor
-
-        glib
-        zlib
-        expat
-        openssl
-     ];
-   };
- }
+in pkgs.stdenv.mkDerivation rec {
+  name = "r3-env";
+  buildInputs = [ pkgs.freetype ];
+  LD_LIBRARY_PATH = with pkgs.xlibs; "${pkgs.mesa}/lib:${libX11}/lib:${libXcursor}/lib:${libXxf86vm}/lib:${libXi}/lib";
+}
