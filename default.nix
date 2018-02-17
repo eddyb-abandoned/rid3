9741c7f3d1316981b8aa9f45146e150ceb3bf0ee
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2018-02-17 15:55:51 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
    pkgs = import <nixpkgs> {};
 in pkgs.stdenv.mkDerivation rec {
-  name = "r3-env";
+  name = "rid3-env";
   buildInputs = [ pkgs.freetype ];
   LD_LIBRARY_PATH = with pkgs.xlibs; "${pkgs.mesa}/lib:${libX11}/lib:${libXcursor}/lib:${libXxf86vm}/lib:${libXi}/lib";
 }
